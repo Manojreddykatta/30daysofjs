affdcacef689dbd02215158c1c0e839b13dfb66c
Manojreddykatta <manojkatta97@gmail.com>
2022-12-21 08:32:10 +0530

    Day-01 working with external javscript files

---
+++ Day-01/main.js
@@ -0,0 +1,19 @@
+let firstName = 'Manoj Reddy';
+let lastName = 'Katta';
+let age='25';
+
+let isMarried = false;
+let weight = 76.8;
+
+
+
+
+console.log("Firstname is "+ firstName + "  " +"Lastname is "+ lastName );
+console.log("Age is " + age );
+
+
+let maritualStatus = isMarried;
+if (isMarried == true )
+    {console.log("Unmarried");}
+else
+    {console.log("Married");}
