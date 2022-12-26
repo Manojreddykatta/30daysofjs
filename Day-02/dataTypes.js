087a1d801de191066251512c40edb8d069c0cb43
Manojreddykatta <manojkatta97@gmail.com>
2022-12-26 10:02:45 +0530

    Data types - numbers

---
+++ Day-02/dataTypes.js
@@ -0,0 +1,58 @@
+let numberOne = 3;
+let numberTwo = 3;
+
+console.log (numberOne == numberTwo);
+
+let js = 'javaScript'
+let py = 'python'
+
+console.log ( js == py );
+
+let nums = [3,5,6]
+nums[1]=13
+
+console.log(nums)
+
+let numbs=[1,2,3]
+let numbers=[1,2,3]
+console.log(numbers == nums)
+
+let userOne ={
+    name:'Manoj',
+    role:'Devops Engineer',
+    country:'India'
+}
+let userTwo ={
+    name:'Manoj',
+    role:'Devops Engineer',
+    country:'India'
+}
+console.log(userOne == userTwo)
+
+let age=35;
+let gravity=9.81;
+let mass=72;
+
+// const PI = 3.14;
+const boilingPointOfWater = 100;
+const bodyTemp = 37 ;
+
+console.log(age,gravity,mass,boilingPointOfWater,bodyTemp);
+
+const PI = Math.PI;
+console.log(PI);
+
+console.log(Math.round(PI));
+console.log(Math.round(9.81));
+console.log(Math.round(PI));
+console.log(Math.ceil(PI));
+console.log(Math.floor(PI));
+console.log(Math.min(25,7,1997,1,4,88,33));
+console.log(Math.max(25,7,1997,1,4,88,33));
+
+const randNumber = Math.random();
+console.log(randNumber);
+
+const randNumber1 = Math.floor(Math.random()*11);
+console.log(randNumber1);
+
