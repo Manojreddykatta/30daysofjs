10c38e2cb8acb3505a764b73a384a6436a60f1a0
Manojreddykatta <manojkatta97@gmail.com>
2023-01-04 21:36:34 +0530

    alert and confirm window methods

---
+++ Day-03/operators.js
@@ -0,0 +1,20 @@
+//increment operator
+let count = 0
+console.log(++count);
+console.log(count++);
+// decrement operator
+console.log(count--);
+console.log(--count);
+
+// ternary operator
+let isRaining = true;
+isRaining
+? console.log('you need a raincoat')
+: console.log('No need for a rain coat.');
+
+let isRaining2 = false;
+isRaining2
+? console.log('you need a raincoat')
+: console.log('No need for a rain coat.');
+
+
